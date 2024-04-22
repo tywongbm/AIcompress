46fa0fffcbab1a8c7bb915798e8a57f4cf6fdf69
tywongbm <jacky.tywong@outlook.com>
2024-04-22 19:09:45 +0800

    modify server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 import io
 from process import modify_image
+import subprocess
 
 
 app = Flask(__name__)
@@ -12,15 +13,25 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    processed_image = modify_image(file)
-    processed_image.save('test.jpg')
+    file.save('input_image.jpg')
 
+
+    ###### Modify the following code to test your model ######
+    command = "python lossy/test.py --model lossy/model/model_celeba.pth --img input_image.jpg --mask lossy/mask.png --output lossy/output --merge"
+    subprocess.call(command, shell=True)
+    output_path = 'lossy/output/result/result-input_image-mask.png'
+
+
+
+    return send_file(output_path, mimetype='image/jpeg', as_attachment=True, download_name='process_iamge.jpg')
+
+'''
     output_buffer = io.BytesIO()
     processed_image.save(output_buffer, format='JPEG')
     output_buffer.seek(0)
 
     return send_file(output_buffer, mimetype='image/jpeg', as_attachment=True, download_name='process_iamge.jpg')
-
+'''
 
 if __name__ == '__main__':
     app.run()
